5946d6fb4d11ca44108a63aa891eeec41575d0c6
Andrea Miglietta <andrea.miglietta92@gmail.com>
2017-04-07 16:53:43 +0200

    Update i3blocks.conf

--- .config/i3/src/cpu.bash
+++ .config/i3/src/cpu.bash
@@ -5,7 +5,6 @@ awk '/Average/{
     cpu = $3+$5
     printf("%.2f%%\n", cpu)
     printf("%.2f%%\n", cpu)
-    if (cpu >= 70) printf("#FF00FF\n") 
-    else           printf("#00FFFF\n")
+    if (cpu >= 70) printf("#FF00FF\n")
 }'
 
